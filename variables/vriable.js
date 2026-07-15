887cb833b772f3020d3aac10257f3c09298079c5
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-15 10:08:39 +0530

    3 type of variable in js

---
+++ variables/vriable.js
@@ -0,0 +1,17 @@
+// variable ek container hota hai jisme hum data store kar skte hai 
+// REAL LIFE EXAMPLE
+
+// Bottle -> pani store krna 
+// bag -> book store krna 
+
+// vese hi variable data store krte hai 
+
+// JS ME VARIBALE BNANANE KE 3 TRIKE HOTE HAI 
+
+// 1 => let (most commom)
+// 2 => var (old)
+// 3 => const (if value doesn`t change in life)
+
+var age  = 20 ;
+let age = 30;
+const pi = 3.14;
