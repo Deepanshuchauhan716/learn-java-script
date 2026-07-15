8b62db3a440087cafe4886d1c866ab0b254f989c
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-15 11:07:16 +0530

    remove comment

--- variables/variablescope.js
+++ variables/variablescope.js
@@ -13,36 +13,36 @@
 // 1 ===> GLOBAL SCOPE 
 // jo variable sab jagah use ho sakta hai 
 
-// let name = "deepanshu";
+let name = "deepanshu";
 
-// function greet (){
-//     console.log(name);
-// }
+function greet (){
+    console.log(name);
+}
 
-// console.log(name);
+console.log(name);
 
-// greet();
+greet();
  
 // 2 ===> BLOCK SCOPE 
 // BLOCK MEANS { } KE ANDR KA AREA
 
-// if(true){
-//     let age  = 20;
-// }
-// console.log(age);
+if(true){
+    let age  = 20;
+}
+console.log(age);
 
 //error kyoki age block ke andr hai isliye hum use {} block ke bhar use nhi kr skte 
 
 // 3 ===> FUNCTION SCOPE 
 // function ke andr bnaya gya variable sirf usi function ke andr use hota hai
 
-// function test(){
-//     let city = "delhi";
-//     console.log(city);
-// }
+function test(){
+    let city = "delhi";
+    console.log(city);
+}
 
-// test();
-// console.log(city);
+test();
+console.log(city);
 
 // output ==> 
     // delhi
