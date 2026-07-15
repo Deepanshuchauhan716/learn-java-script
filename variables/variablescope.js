3fd3bf278793ee67c341dc51ba045b813831d4c5
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-15 10:54:50 +0530

    types of scope with example

--- variables/variablescope.js
+++ variables/variablescope.js
@@ -22,3 +22,28 @@ function greet (){
 console.log(name);
 
 greet();
+ 
+// 2 ===> BLOCK SCOPE 
+// BLOCK MEANS { } KE ANDR KA AREA
+
+if(true){
+    let age  = 20;
+}
+console.log(age);
+
+//error kyoki age block ke andr hai isliye hum use {} block ke bhar use nhi kr skte 
+
+// 3 ===> FUNCTION SCOPE 
+// function ke andr bnaya gya variable sirf usi function ke andr use hota hai
+
+function test(){
+    let city = "delhi";
+    console.log(city);
+}
+
+test();
+console.log(city);
+
+// output ==> 
+    // delhi
+    // error 
