65a2bece02abca3c99bef4e2df67dd9b16366f58
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-15 11:17:45 +0530

    3 question complete

--- variables/variablepracticeque.js
+++ variables/variablepracticeque.js
@@ -1,4 +1,19 @@
-// 1 question ====> ek variable name bnao aur usme apna name store kro for print krvao 
+// 1 question ====> ek variable name bnao aur usme apna name store kro for print krvao ?
 
 let name = "deepanshu";
 console.log(name);
+
+// 2 question ====> 3 variable bnaao sbko ek hi console ki help se print krvao ?
+
+let naam = "deepanshu";
+var age =  20;
+const city = "delhi";
+
+console.log(name,age,city);
+
+// 3 question ====> ek variable marks bnao uske baad uski value update krke print krvao
+
+let marks = 40;
+marks = 100;
+console.log(marks);
+
