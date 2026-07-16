ec83e29767ec101f0b95202da78ab2407d8f4037
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-16 22:21:09 +0530

    Data types in js

---
+++ data_types/datatype.js
@@ -0,0 +1,14 @@
+// Data types In JS
+
+// data types btate hai ki kis variable ke andr kis type data store hoga 
+// EXAMPLE
+
+let age =  20; // yaha 20 ek number hai
+let name = "deepanshu" // yaha deepanshu ek string hai'
+
+// TWO TYPES OF DATA TYPE EXIST IN JS
+// 1 => PREMETIVE DATA TYPE . 2 => NON-PREMETIVE DATA TYPES
+
+// PREMETIVE
+// 1 => NUMBER
+
