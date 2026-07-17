988420301d739a15f5e1eb041e45ab7eea37e7ff
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-17 22:30:45 +0530

    operator in js

---
+++ operator/operator.js
@@ -0,0 +1,56 @@
+// operator ek special symbol hota hai joo value ya variable par operation karta hai
+
+// EXAMPLE
+
+let a = 10;
+let b = 20;
+
+let sum = a + b // + , = is the operator here .
+
+// TYPES OF OPERATOR IN JAVA SCRIPT 
+// 1 => arithmatic operator
+
+let a = 10;
+let b = 20;
+
+console.log(a + b); //addition operator
+console.log(a - b); //subtraction operator
+console.log(a * b); // multiply operator
+console.log(a / b); // devide operator
+console.log(a % b); // modulo operator
+console.log(a ** b); // power operator
+
+// 2 => assignment operator  value ko assign krne ke liye 
+
+let x = 10;
+x += 5
+x -= 3
+x *= 2
+x /= 4
+x %= 1
+
+
+// 3 => comparision operator 2 ya more value ko compare krne ke liye
+
+let a = 10;
+let b = 20
+ 
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
+console.log(a != b)
+console.log(a === b)
+console.log(a !== b)
+
+// 4 => logical operator
+// 5 => increment and decrement operator
+// 6 => string operator
+// 7 => turnary operator
+// 8 => nullish coalescing operator
+// 9 => optional chaining operator
+// 10 => type operator
+// 11 => bitwise operator
+
+
