436820fdbab631b52b2939381fd2f8238cab7ad6
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-18 21:10:25 +0530

    need of typeconversion

--- typeconversion/typeconversion.js
+++ typeconversion/typeconversion.js
@@ -5,3 +5,13 @@ let age = "20";
 // yaha 20 ek string hai 
 
 let age = Number("20"); //ab ye string nhi blki ek number me convert ho gya
+
+// this is called type conversion
+
+// why need to convert data type 
+// agar tum kisi user se age input lete ho
+
+let age = prompt("Enter your age ");
+
+// lekin prompt humesa string return krta hai
+// In memory => age = "20"
