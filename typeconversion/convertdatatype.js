299a1fb3ed670e4a25c86b6d6a317cc47a39335b
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-18 21:30:34 +0530

    convert data type

--- typeconversion/convertdatatype.js
+++ typeconversion/convertdatatype.js
@@ -4,3 +4,21 @@ let age = "20";
 let num = Number(age);
 
 console.log(num);
+
+// STRING TO BOOLEAN
+
+let a = Boolean("hello");
+console.log(a); //true
+
+let b = Boolean("");
+console.log(b); // false
+
+//NUMBER TO STRING 
+
+let st = String(100);
+console.log(st) //100 is a string here not a number 
+
+// BOOLEAN TO STRING
+
+let bool = String(true);
+console.log(bool);
