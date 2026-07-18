b9af6f8e5378c4690156408623e4c56fdf527abe
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-18 21:06:02 +0530

    typeconversion in js

---
+++ typeconversion/typeconversion.js
@@ -0,0 +1,7 @@
+// Basically typecenversion ka matlab hota hai ek data type ko dusre data type me convert karna
+
+// example 
+let age = "20";
+// yaha 20 ek string hai 
+
+let age = Number("20"); //ab ye string nhi blki ek number me convert ho gya
