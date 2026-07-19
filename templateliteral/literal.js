0662baa90077fd9174289592ce08f26f898f5aba
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-19 12:10:37 +0530

    Template literal

---
+++ templateliteral/literal.js
@@ -0,0 +1,12 @@
+// TEMPLATE LITERAL EK SPECIAL TYPE KI STRING HOTI HAI 
+// IISE HUM
+
+// => VARIABLE KO STRING KE ANDR DAAL SKTE HAI
+// =>EXPRESSION KO CALCULATE KRKE STRING ME DIKHA SKTE HAI
+// =>MULTI LINE STRING LIKH SKTE HAI
+
+// ==========================================String banane ke 3 trike hote hai ======================
+
+  let name = 'deepanshu';// ================ 1=> SINGLE QUOTES 
+  let name = "deepanshu";// ================ 2=> DOUBLE QUOTES
+  let name = `deepanshu`;// ================ 3=> TEMPLATE LITERAL (BACKTICKS)
