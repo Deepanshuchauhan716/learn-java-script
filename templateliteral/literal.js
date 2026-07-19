10a3435ab5b1dbd45fa042ceafdad3407817aed3
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-19 12:17:44 +0530

    need of template literal

--- templateliteral/literal.js
+++ templateliteral/literal.js
@@ -8,5 +8,20 @@
 // ==========================================String banane ke 3 trike hote hai ======================
 
   let name = 'deepanshu';// ================ 1=> SINGLE QUOTES 
-  let name = "deepanshu";// ================ 2=> DOUBLE QUOTES
-  let name = `deepanshu`;// ================ 3=> TEMPLATE LITERAL (BACKTICKS)
+  let name1 = "deepanshu";// ================ 2=> DOUBLE QUOTES
+  let name2 = `deepanshu`;// ================ 3=> TEMPLATE LITERAL (BACKTICKS)
+
+
+//   =======NEED OF TEMPLATE LITERAL
+
+let nam = "deepanshu";
+let age = 20;
+let city = "delhi";
+
+console.log("my name is " + nam + "I am " + age + "Year old and i live in " + city);
+
+// so this code is not clean and very tricky now enter the template literal
+
+console.log(`my name is ${nam}, I am ${age} year old and i live in ${city}`);
+
+// =============SO THIS IS THE DIFFERENCE ===============
