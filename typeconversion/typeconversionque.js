931228bf7c7c86a3f4f25c84864517e512021c05
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-19 11:32:59 +0530

    level2 complete

--- typeconversion/typeconversionque.js
+++ typeconversion/typeconversionque.js
@@ -10,3 +10,16 @@ console.log("5" * 5) // 25  beacause * operator bhi concate krne ke jgh number b
 console.log("20" / 4) // 5  beacause / operator bhi concate krne ke jgh number banane ki kosis krta hai
 
 console.log("20" % 3) // 2 beacause % operator concate krne ke jgh number banane ki kosis krta hai
+
+// LEVEL 2 BOOLEAN
+
+console.log(true + 5) // 6 beacuse true =  1 so java script as a number treat kr rhi hai true ko so 5 + 1 = 6
+
+console.log(false + 5) // 5 beacause false = 0 
+
+console.log(true + true) //2 beacause 1 + 1 = 2
+
+console.log(true + false) // 1
+
+console.log(true * 10) // 10 
+
