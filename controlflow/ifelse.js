1d123a7fffc2ff7208bef454bcf6d972f733576c
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-21 11:39:54 +0530

    control flow in js

---
+++ controlflow/ifelse.js
@@ -0,0 +1,25 @@
+// BASICALLY CONTROL FLOW KA MATLAB HOTA HAI PROGRAM KIS ORDER ME EXECUTE HOGAA AUR KONSE CODE KAB CHLEGA THIS IS CALLED CONTROL FLOW!!
+
+// EXAMPLE======> (normal flow)
+    console.log("A");
+    console.log("B");
+    console.log("C");
+
+// agar decision lena ho tabh
+
+let age = 20;
+
+if (age >= 18){
+    console.log("you can vote!!")
+} 
+    
+// yahaan js pehle condition check kregi agar condition true hui tbhi code chlega if ke andr ka 
+// aur agar false hui to hum use krenge else
+
+if( age >= 18){
+    console.log("vote!")
+}else{
+    console.log("you are not eligible");
+}
+
+// agar if ki condition false hui to else ki condition print hogi
