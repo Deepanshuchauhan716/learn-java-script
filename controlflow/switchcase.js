f26999df34f4670d556a09d09011fbaee072847b
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-21 13:10:45 +0530

    switch statement

---
+++ controlflow/switchcase.js
@@ -0,0 +1,56 @@
+// switch case ka use tab hota hai jab ek hi variable koo bahut saari fixed value se compare krna ho
+
+// socho restaurant me manu hai 
+// 1 -> pizza
+// 2 -> burger
+// 3 -> pasta
+
+// ab js ko dekhna pdega user ne konsa number choose kiya 
+
+// ======================== syntax ======================
+
+
+//  switch(expression){
+//     case 1:
+//         code
+//         break
+
+//     case 2:
+//         code
+//         break
+
+//     case 3:
+//         code
+//         break
+
+//     case 4:
+//         code
+//         break
+//  }
+
+let day = 2;
+
+switch(day){
+
+    case 1:
+        console.log("mon");
+        break
+    case 2:
+        console.log("tue");
+        break
+    case 3:
+        console.log("wed");
+        break
+    case 4:
+        console.log("thu");
+        break
+    case 5:
+        console.log("fri");
+        break
+    case 6:
+        console.log("sat");
+        break
+    case 7:
+        console.log("sun");
+        break
+}
