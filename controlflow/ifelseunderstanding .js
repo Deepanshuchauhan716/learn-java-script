d2ede8c7601a22146aae07a4cce04b0a03fc4c9c
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-21 12:12:09 +0530

    if statement

---
+++ controlflow/ifelseunderstanding .js
@@ -0,0 +1,18 @@
+// IF EK CONDITION STATEMENT HOTI HAI 
+// AGAR CONDITION TRUE HAI TO CODE CHALWAO WARNA SKIP KRDO
+
+// REAL LIFE EXAMPLE
+// AGAR BAARISH HUI -> TO CHATA LE JAAO
+
+// SYNTAX
+// if(condition){
+//  ye code tbhi chlega jab condition true hogi
+// }
+
+//  ==================== example 1 ===============
+
+let price = 2000;
+
+if(price >= 1800){
+    console.log("expensive");
+}
