7704556bfbd0d6705bdd6dc6c4874f99ef6165fd
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-21 12:16:37 +0530

    truthy and falsy value

--- controlflow/ifelseunderstanding .js
+++ controlflow/ifelseunderstanding .js
@@ -16,3 +16,26 @@ let price = 2000;
 if(price >= 1800){
     console.log("expensive");
 }
+
+// ===================example 2 ======================
+
+let age = 15;
+if(age <= 18){
+    console.log("baby");
+}
+
+// truthy value==================
+
+// if(100)
+// if("Hello")
+// if([])
+// if({})
+
+// falsy value ==================
+
+// if(0);
+// if("")
+// if(null)
+// if(undefined)
+// if(NaN)
+
