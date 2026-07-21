f872d9b9d1e7855aaae9b8b2fd1e272b4a844fc6
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-21 12:54:29 +0530

    Grade system

---
+++ controlflow/practiceifelse.js
@@ -0,0 +1,20 @@
+// GRADE SYSTEM
+
+let marks = 85;
+let name = "deepanshu";
+
+if(marks >= 90){
+    console.log(`A grade ${name}`)
+}
+else if(marks >= 80){
+    console.log(`B grade ${name}`)
+}
+else if(marks >= 75){
+    console.log(`C grade ${name}`)
+}
+else if(marks >= 50){
+    console.log(`D grade ${name}`)
+}
+else{
+    console.log(`fail ${name} sorry`);
+}
