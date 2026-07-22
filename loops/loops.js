755a616f8f091e7c202930b9c62e13ecd52b736a
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-22 11:07:10 +0530

    loops

---
+++ loops/loops.js
@@ -0,0 +1,19 @@
+// LOOP KA BASIC MATLAB HOTA HAI EKI CODE KO BAAR BAAR EXECUTE KRNA JB TAK KOI CONDITION TRUE HO
+
+// =================================== EXAMPLE ===============================================
+
+// Maanlo tumhe 100 bar likhna hai hello 
+
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+
+// this is very tricky and bad practice
+// ab hum ese situation ke liye use krte hai loops ka
+
+for(let i = 0; i <= 100; i++){
+    console.log(`${i} Hello`);
+}
+
+// this is very simple and easiest way to execute one code more time
