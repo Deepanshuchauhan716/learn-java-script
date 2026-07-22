335c2aca7fd35544f7dd961c4fee13266a1f4f47
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-22 11:21:30 +0530

    type of loops with example

--- loops/loops.js
+++ loops/loops.js
@@ -27,3 +27,56 @@ for(let i = 0; i <= 100; i++){
 //  5 games
 //  6 data process and etc work
 
+// =========================================== TYPES OF LOOPS IN JAVA SCRIPT =========================
+
+// 1 FOR LOOP
+// 2 WHILE LOOP
+// 3 DO WHILE LOOP
+// 4 FOR OF LOOP
+// 5 FOR IN LOOP
+
+
+// --------------------------------------- for loop -----------------------------
+
+// Syntax
+
+                          // for( initialization; condition ; update){
+                                     //code
+                                // }
+
+// ###################################  EXAMPLE ##########################
+
+for(let i = 1; i <= 5; i++){
+    console.log("Deepanshu");
+}
+
+
+
+//  ---------------------------------------- WHILE LOOP ---------------------------
+// ye loop pahle condition check krta hai agar condition true hai tabhi loop chlega
+
+// syntax
+                        // while(condition){
+                                // code 
+                            // }
+
+//   ===================================== EXAMPLE =================================
+
+ let i = 1;
+ while(i <= 5){
+    console.log(i);
+    i++;
+ }
+
+
+//  -------------------------------- DO-WHILE LOOP -------------------------------------
+
+// ISME CODE KAM SE KAM EK BAAR EXECUTE HOTA HAI 
+
+// %%%%%%%%%%%%%%%%%%%%%% EXAMPLE %%%%%%%
+
+let a = 10;
+do{
+    console.log(a)
+} while(a < 5); // condition false thi fir bhi code ek baar chla
+
