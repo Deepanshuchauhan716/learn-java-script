f566e4e21699ff7e5466ac57f5ae349e624cbb61
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-23 10:54:02 +0530

    function declaration??

---
+++ function&object/funcDeclaration.js
@@ -0,0 +1,18 @@
+// FUNCTION DECLARATION KA MATALB HOTA HAI EK TRIKE SE FUNCTION BANANA JISME FUNCTION KEYWORD KA USE HOTA HAI
+
+// BASIC SYNTAX
+
+function FunctionName(){
+    // function body
+
+    return value; // optional
+}
+
+// ============= example ===========
+
+function greet (name){
+    return `hello ${name}`;
+}
+
+console.log(greet("Deepanshu")); // hello deepanshu
+
