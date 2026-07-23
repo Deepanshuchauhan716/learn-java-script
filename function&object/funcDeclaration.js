622c45e31010fc1cd430ab1a655d4390c2810e07
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-23 11:12:06 +0530

    parameter and return

--- function&object/funcDeclaration.js
+++ function&object/funcDeclaration.js
@@ -16,3 +16,42 @@ function greet (name){
 
 console.log(greet("Deepanshu")); // hello deepanshu
 
+//  ========================================= FUNCTION EXPRESSION ==================================
+
+// FUNCTION EXPRESSION KA MATLAB HOTA HAI JAB HUM FUNCTION KO EK VALUE KI TRH VARIABLE ME STORE KRTE HAI 
+
+const functionName = function(parameter){
+    //function body
+ };
+
+// ================================= parameter and return =========================
+
+// parameter wo variable hainn jo fucntion define krte time parantheses() mai likhe jaate hai ye function ko input recieve krne ke kaam krte hai
+
+function functionName(parameter1,parameter2){
+    //function body
+}
+
+// ========================== example =======================
+
+function add(a,b) { // a aur b parameter hai 
+    return a + b
+}
+
+add(2,5);// 2 aur 5 argument hai 
+
+
+// ================================ Return statement ====================================
+
+// return function se value wapas bhejta hai aur function ko immidiatly stop kar deta hai
+
+// Basic syntax
+
+function substract(a,b){
+    return a - b;
+}
+
+const result = substract(5,3);
+console.log(result); // 2
+
+
