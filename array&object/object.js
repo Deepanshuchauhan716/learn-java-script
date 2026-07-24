360b54013bdb019061583f773660decb25e1e147
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-24 13:23:17 +0530

    object

---
+++ array&object/object.js
@@ -0,0 +1,18 @@
+// 🫡🫡 object MOST important topic
+
+// object => object ek collection hota hai jisme related data aur function ko ek saath store kiya jaata hai
+// maanlo ek student ke baare me
+// Name
+// age
+// city
+
+// ye sab ek student ki information hai isliye hum inhe store krne  ke liye object bnaate hai
+
+// ########## syntax ############
+
+const objectName = {
+    //code here
+}
+
+// {} ko object literal khte hai
+
