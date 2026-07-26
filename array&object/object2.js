6624f15ddf2341c2b8226abcc58e3109db56ebfd
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-26 11:07:33 +0530

    objects basic

---
+++ array&object/object2.js
@@ -0,0 +1,29 @@
+// object ek container hota hai jisme hum kisi ek cheez ki related information ko ek saath store kr skte hai
+
+// example
+// kisi student se related
+
+// name => rohit
+// class => 12th
+// city => delhi
+// country => india 
+
+// ye sb eki student ka data ya info hai isliye ise store krne ke liye hum object ka use krte hai
+
+// object syntax
+
+const object = {
+    // code here
+}
+
+// yaha {} ko object khte hai
+
+// object me data kaise store krte hai
+
+const student = {
+    name : "Rohit",
+    age : 20,
+    city : "Delhi",
+    country:"India"
+};
+
