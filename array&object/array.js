a65b3c910747e8079ac0ed430752c31f971c9369
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-28 10:41:08 +0530

    array

---
+++ array&object/array.js
@@ -0,0 +1,19 @@
+// *********************** ARRAY **************************** //
+
+// ARRAY EK SPECIAL OBJECT HAI JO EK HI VARIABLE ME MULTIPLE VALUE KO ORDER KE SAATH STORE KARTA HAI
+
+// ****** WITHOUT ARRAY *********//
+
+let s1 = "Deep";
+let s2 = "Rohit";
+let s3 = "Aman";
+let s4 = "Ankit"; // bahut saare variable banane pde
+
+// **** *** WITH ARRAY **** *** //
+
+const studentName = ["Deep","Rohit","Rahul","Aman","ankit"]; // ek hi variable me sab values aa gye
+
+
+
+
+
