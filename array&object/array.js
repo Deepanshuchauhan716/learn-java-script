866dce7b27e7476ecd09a67e6090b0bcfd3011e9
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-07-28 10:50:39 +0530

    index value & element access

--- array&object/array.js
+++ array&object/array.js
@@ -13,6 +13,31 @@ let s4 = "Ankit"; // bahut saare variable banane pde
 
 const studentName = ["Deep","Rohit","Rahul","Aman","ankit"]; // ek hi variable me sab values aa gye
 
+// [] => array
+// "Rohit","Deep" => Element
+// array ke andr jo bhi value hoti hai use element kehte hai
+
+const number = [12,34,45,67,89];
+console.log(number);
+
+// * * * * INDEX * * * * //
+
+// index value kya hoti hai?? 
+// jab bhi hum array me koi element store krte hai to har ek element ko ek index value milti hai index value 0 se shuru hoti hai
+
+// jaise const number = [12,34,45,67,89];  isme 12 ko 0 index milega fir 34 ko 1 fir 45 ko 2 ese hi aage bdta rhega
+
+// * * * * * * * * * * * ELEMENT ACCESS KRNA * * * * * * * * * //
+
+const fruit = ["Apple","banana","Mango","litchi"];
+console.log(fruit[1]); // 1 ka matlab yaha index value se hai kyoki 1 index pe banana hai to hume console krke banana milega
+
+// multiple value access
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
+
+
 
 
 
