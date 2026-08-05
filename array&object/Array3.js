eb91043097b82f308655f0d56afd1641d5646b73
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-08-05 12:34:15 +0530

    Array

---
+++ array&object/Array3.js
@@ -0,0 +1,12 @@
+// ******************************** ARRAY ********************************** //
+
+// ARRAY JS ME EK SPECIAL VARIABLE HOTA HAI JO EK SAATH BAHUT SAARI VALUE KO STORE KR SKTA HAI
+
+// NORMAL VARIABLE ME AAM TAUR PAR EK HI VALUE STORE HOTI HAI
+
+let name = "Deepanshu";
+
+// lekin agar tumhe kai value ko ek hi jgh rakhna hai to array use krte hai
+
+let fruits = ["Apple","Mango","Banana"]; 
+// fruits ek array hai , jisme 3 value ek saath rkhi gyi hai 
