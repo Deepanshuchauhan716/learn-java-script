68efd61ca1f003aac314aa657b002fde94a0372a
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-08-05 12:40:20 +0530

    Array index value

--- array&object/Array3.js
+++ array&object/Array3.js
@@ -10,3 +10,21 @@ let name = "Deepanshu";
 
 let fruits = ["Apple","Mango","Banana"]; 
 // fruits ek array hai , jisme 3 value ek saath rkhi gyi hai 
+
+
+//  ************************ INDEX *********************************** //
+
+// index ka matlab hota hai array ke andr har value ka address ya position number 
+// jaise ek train me har seat ka apna seat number hota hai waise hi array har value ka apna index hota hai
+
+let student = ["Rohit","chetan","Deepanshu","Anu"];
+
+// is student array me har ek value ki apni position hai un position ko INDEX kehte hai 
+
+// Rohit ka index value 0 hai
+// chetan ki 1
+// deepanshu ki 2
+// anu ki 3
+
+// index humesa 0 se shuru hota hai  isliye phli value ka index humesa 0 hota hai
+
