2ebb94473652d07017591671fa7cd333a7d51165
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-09-05 14:31:27 +0530

    add alert

--- project.js/NotesApp.js
+++ project.js/NotesApp.js
@@ -2,14 +2,29 @@ let addNote = document.getElementById("addNewNote");
 let popup = document.querySelector(".popup");
 let cut = document.querySelector(".cut");
 
+let tittleinp = document.getElementById("tittle_input");
+let discriptioninp = document.getElementById("discription_input")
+let noteform = document.getElementById("noteForm");
 
-// Add note box par click
 addNote.addEventListener("click", function () {
     popup.classList.add("show");
 });
 
 
-// × par click
 cut.addEventListener("click", function () {
     popup.classList.remove("show");
+});
+
+noteform.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    let tittle = tittleinp.value
+    let discription = discriptioninp.value;
+
+    if(tittle === "" || discription === ""){
+        alert("please fill the tittle and discription");
+        return;
+    }
+
+    
 });
