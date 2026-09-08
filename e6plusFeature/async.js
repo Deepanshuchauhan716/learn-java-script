ac18484965c3e84119cfbf782ecae450ba0cfa75
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-09-08 09:44:57 +0530

    async and await

--- e6plusFeature/async.js
+++ e6plusFeature/async.js
@@ -41,3 +41,48 @@ console.log("four")
 // agar code sync me chlta to phle one two print hota fir js 4 second wait krti jab tk 4 second me hello print na ho jaaye fir three and four print hote par js me esa nhi hua 
 
 // ise kehte hai asynchrinous programming
+
+// async kya karta hai?
+
+// Jis function ke andar hum await use karna chahte hain, uske aage async lagate hain.
+
+// async = is function ke andar asynchronous kaam handle kar sakte hain.
+
+
+function getUser(){
+   let obj = {
+    name : "Deepanshu",
+    course : "BCA"
+   };
+
+   return obj;
+};
+
+async function showUser() {
+
+    let user = await getUser();
+
+    console.log(user);
+}
+
+showUser();
+
+
+function getUse() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                name: "Deepanshu",
+                course: "BCA"
+            });
+        }, 2000);
+    });
+}
+
+async function showUser2(user2) {
+    let use = await getUse();
+
+    console.log(use)
+}
+
+showUser2();
