9e6dde9ca6366932076803f624369f42f32c4dff
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-09-18 12:05:23 +0530

    Catch all inputs

---
+++ project.js/create.js
@@ -0,0 +1,7 @@
+// sbhi input ko cathch kro >>>>
+const signUp = document.getElementById('signup_form');
+const userName = document.getElementById('username');
+const Email = document.getElementById('email');
+const passWord = document.getElementById('password');
+const ConfPass = document.getElementById('conforim');
+
