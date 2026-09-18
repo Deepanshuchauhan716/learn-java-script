4c8f0577b250209c954ac5f2cc3c6a8c4ae9a066
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-09-18 12:45:48 +0530

    Catch all input val and check email password valid or not

--- project.js/create.js
+++ project.js/create.js
@@ -1,7 +1,34 @@
 // sbhi input ko cathch kro >>>>
 const signUp = document.getElementById('signup_form');
 const userName = document.getElementById('username');
-const Email = document.getElementById('email');
+const email = document.getElementById('email');
 const passWord = document.getElementById('password');
-const ConfPass = document.getElementById('conforim');
+const confPass = document.getElementById('conforim');
 
+signUp.addEventListener("submit",(e) =>{
+    e.preventDefault(); // page reload hone se rokta hai
+
+    // ab saari values ko uthaao input se
+    const userNameVal = userName.value.trim();
+    const emailVal = email.value.trim();
+    const passVal = passWord.value;
+    const confPassVal = confPass.value;
+
+    if(userNameVal == ""|| emailVal == "" || passVal== "" || confPassVal == ""){
+        alert("Please fill the form");
+    }
+
+    const emailPattern =   /^[^\s@]+@gmail\.com$/;
+    if(!emailPattern.test(emailVal)){
+        alert("Enter Valid Email");
+    }
+
+    const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if(!passPattern.test(passVal)){
+        alert("Password must be at least 8 characters and contain uppercase, lowercase, number and special character");
+        return;
+    }
+
+    console.log(userNameVal,emailVal,passVal,confPassVal);
+
+})
